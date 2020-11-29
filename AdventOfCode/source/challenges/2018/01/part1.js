5539d833936d5198561d94364edb88cf8f3500a8
mrobitaille <mrobitaille@stingray.com>
2020-11-29 14:36:04 -0500

    solved 2019-01

---
+++ AdventOfCode/source/challenges/2018/01/part1.js
@@ -0,0 +1,16 @@
+let lib = require('../../../lib');
+
+let year = 2018;
+let day = 01;
+
+lib.getInput(year, day).then((data) => {
+    let lines = data.split('\n');
+    for(let line of lines) {
+        
+    }
+
+    console.log("-- Writing output --");
+    lib.writeOutput(output);
+}).catch((err) => {
+    console.log(err, err.stack);
+});
---
+++ AdventOfCode/source/lib/Output.js
@@ -0,0 +1,39 @@
+let fs = require('fs');
+let path = require('path');
+
+
+function writeOutput(data){
+    let currentDirectory = path.dirname(require.main.filename);
+    let outputFileName = path.resolve(currentDirectory, 'output.txt');
+        fs.stat(outputFileName, (err, stats) => {
+        if(fs.existsSync(currentDirectory)) {
+            fs.writeFile(outputFileName, data, (err) => {
+                if(err) throw err;
+                console.log(" |- Wrote ->", currentDirectory);
+            })
+        } else {
+            console.log(" |- >", currentDirectory)
+            console.log(err);
+        }
+    });
+}
+
+function appendOutput(data){
+    // change this to append
+    let currentDirectory = path.dirname(require.main.filename);
+    let outputFileName = path.resolve(currentDirectory, 'output.txt');
+        fs.stat(outputFileName, (err, stats) => {
+        if(fs.existsSync(currentDirectory)) {
+            fs.writeFile(outputFileName, data, (err) => {
+                if(err) throw err;
+                console.log(" |- Wrote ->", currentDirectory);
+            })
+        } else {
+            console.log(" |- >", currentDirectory)
+            console.log(err);
+        }
+    });
+}
+
+module.exports = writeOutput;
+module.exports = appendOutput;
