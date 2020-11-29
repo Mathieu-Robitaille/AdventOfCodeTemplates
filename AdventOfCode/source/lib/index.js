b6179f89fd23f222f2d9a2391e96117330121b67
Mathieu Robitaille <mrobitialle@gmail.com>
2020-11-29 10:06:30 -0500

    need to display directory listing

--- AdventOfCode/source/lib/index.js
+++ AdventOfCode/source/lib/index.js
@@ -1,7 +1,4 @@
 module.exports = {
     getInput: require('./getInput'),
     writeOutput: require('./writeOutput'),
-//     permutations: require('./permutations'),
-//     knotHash: require('./knotHash'),
-//     linkedList: require('./linkedList'),
 }
