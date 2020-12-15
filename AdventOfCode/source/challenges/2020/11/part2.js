4c79063a44da7effb8bd44378bf2ef3bf0ae086f
mrobitaille <mrobitaille@stingray.com>
2020-12-15 09:49:35 -0500

    god this one was boring

--- AdventOfCode/source/challenges/2020/11/part2.js
+++ AdventOfCode/source/challenges/2020/11/part2.js
@@ -1,12 +1,14 @@
 let lib = require('../../../lib');
+let cell = require('../lib/cellularAutomata');
 
 let year = 2020;
 let day = 11;
 
 lib.getInput(year, day).then((data) => {
-    let lines = data.split('\n');
-    for(let line of lines) {
-        
+    let cellautomata = new cell.Automata(data);
+    let output = 0;
+    while(output === 0){
+        output = cellautomata.runOnce('part2')
     }
 
     console.log(" ╦ Displaying output ");
